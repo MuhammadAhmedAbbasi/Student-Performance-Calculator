fa667f49abd3024aec77de88c0172dc152351be6
MuhammadAhmedAbbasi <engrahmedabbasi3015@gmail.com>
2024-03-25 12:22:59 +0800

    new

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 import logging
-
+from logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -16,5 +16,12 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
+if __name__=='__main__':
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info('Divide by zero')
+        raise CustomException(e,sys)
 
     
